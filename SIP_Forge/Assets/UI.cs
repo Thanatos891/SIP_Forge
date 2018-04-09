47e6b98381921a032d3e788002ba8d8220ef2863
KNOWN-UNIVERSE\ausmittl <ausmittl@uat.edu>
2018-04-08 18:19:39 -0700

    mege

---
+++ SIP_Forge/Assets/UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+	Text test = null;
+	// Use this for initialization
+	void Start () {
+		test = GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void UpdateTextTo( )
+	{
+		test.text = "Part Selected: ";
+	}
+}
